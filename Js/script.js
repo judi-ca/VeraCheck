b0d4cea4f54c135c45b9863224b927fbbee9c598
Yacinthe <judicaeltdhi@gmail.com>
2026-03-29 00:08:35 +0000

    update

--- Js/script.js
+++ Js/script.js
@@ -13,8 +13,46 @@ window.addEventListener("scroll", () => {
   lastScroll = currentScroll;
 });
 
-// Message pour dire de patienter que la page verifier n'est pas encore disponible
-const verifer = document.querySelector("#verifier");
-verifer.addEventListener("click", () => {
-  alert("Veuillez patienter, la page de vérification n'est pas encore disponible.");
-});
+
+  // Fonction qui ouvre un cours
+
+  function openCourse(id) {
+    // cacher la loanding page & afficher les cours
+    document.querySelector(".landing").style.display = "none";
+    document.querySelector("#coursPage").style.display = "block";
+
+    const content = document.querySelector("#coursContent");
+
+    // Affichage dynamique
+
+    if (id === "img") {
+     content.innerHTML = `
+     <h1>Détection des images manipulées</h1>
+     <p>Apprenez à identifier les images retouchées ou sorties de leur contexte grâce à des techniques d'analyse visuelle et d'outils en ligne.</p>
+     `;
+    }
+
+    if (id === "fact") {
+      content.innerHTML = `
+      <h1>Fact-checking</h1>
+      <p>Découvrez les bases pour vérifier les informations et éviter les fake news sur internet.</p>
+      `;
+    }
+
+
+    // else
+
+      // else{
+      //   content.innerHTML = `Pas disponible !`
+      // }
+  }
+
+
+  openCourse()
+
+  // Function goBack
+
+  function goBack() {
+    document.querySelector(".landing").style.display = "block";
+    document.querySelector("#coursPage").style.display = "none";
+  }
